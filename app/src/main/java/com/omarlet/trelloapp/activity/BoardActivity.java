8c1125f6a4b161273a448fd80079455b99575ab2
Omar Amin <omar_75@hotmail.dk>
2020-09-18 15:44:17 +0200

    Layout for when clicking on the board

---
+++ app/src/main/java/com/omarlet/trelloapp/activity/BoardActivity.java
@@ -0,0 +1,18 @@
+package com.omarlet.trelloapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omarlet.trelloapp.R;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+
+    }
+}
